d1fa3f2860934148b60611b0b91e956d3a369837
pintubeast <abhishek.jk1992@gmail.com>
2023-08-17 23:34:04 +0530

    q3 split parity done

--- aj4119_hw6_q3_2.cpp
+++ aj4119_hw6_q3_2.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 void removeOdd(int arr[], int& arrSize);
-void reverseArray(int arr[],int arrSize);
+void reverseArray(int arr[],int& arrSize);
 
-void reverseArray(int arr[],int arrSize)
+void reverseArray(int arr[],int& arrSize)
 {   int temp;
     for(int i=0;i<arrSize/2;i++)
     {
@@ -34,16 +34,52 @@ void removeOdd(int arr[], int& arrSize)
 
     }
     arrSize -= deleted;
+    cout<<"The removeODD array size is: "<<arrSize<<"\n"
+        <<"The removeODD output array is: ";
+        for(int i=0;i<arrSize;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+}
+void splitParity(int arr[], int& arrSize);
+void splitParity(int arr[], int& arrSize)
+{   int i=0,j =arrSize-1,temp; 
+    while(i<arrSize && j>=0 && i!=j)
+    {    while(arr[i]%2 != 0 && i<j)
+        {
+            i++;
+        }
+
+        while(arr[j] %2 == 0 && j>i)
+        {
+            j--;
+        }
+
+        temp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=temp;
+
+    }
+        cout<<"The splitParity output array is: ";
+        for(int i=0;i<arrSize;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+}
+
+int main()
+{   int arrSize=5;
+    int arr[5]={1,2,3,4,5};
+    //reverseArray(arr,arrSize);
+    //removeOdd(arr,arrSize);
+    splitParity(arr,arrSize);
+
+
     cout<<"The final array size is: "<<arrSize<<"\n"
         <<"The new array is: ";
         for(int i=0;i<arrSize;i++)
         {
             cout<<arr[i]<<" ";
         }
-}
-int main()
-{   int arrSize=4;
-    int arr[4]={1,2,3,4};
-    removeOdd(arr,arrSize);
     return 0;
 }
