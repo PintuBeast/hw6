5906d4e4218ffd1c0b9e61f1ba1fe8fffca3cdef
pintuBeast <abhishek.jk1992@gmail.com>
2023-08-18 12:19:23 +0530

    q4 complete

--- aj4119_hw6_q4.cpp
+++ aj4119_hw6_q4.cpp
@@ -3,38 +3,55 @@
 #include<cstdlib>
 using namespace std;
 
+bool verifyPin(const int PIN[],int userEntry[],int mask[],int pinSize, int maskSize);
+
+bool verifyPin(const int PIN[],int userEntry[],int mask[],int pinSize, int maskSize)
+{
+    bool match=true;
+    for(int i=0;i<pinSize;i++)
+    {   
+       // cout<<"Pin value is "<<PIN[i]<<"  Mask value is: "
+        //    <<mask[PIN[i]] <<" userEntry is: "<<userEntry[i]<<"\n";
+        if(mask[PIN[i]]!=userEntry[i])
+        {
+            match=false;
+            break;
+        } 
+    } 
+    return match;   
+
+}
+
 int main()
 {
     srand(time(0));
-    const int PIN=12345, ARRAY_SIZE=5;
-    int arr[ARRAY_SIZE];
+    const int PIN[]={3,2,4,6,5}, PIN_SIZE=5,MASK_SIZE=10 ;
+    int mask[MASK_SIZE];
     cout<<"Please enter your PIN according to the following mapping: \n"
         <<"PIN: 0 1 2 3 4 5 6 7 8 9\n"
         <<"NUM: ";
     for(int i=0;i<10;i++)
     {  
-        arr[i]=rand() % 3 + 1;
-        cout<<arr[i]<<" ";
+        mask[i]=rand() % 3 + 1;
+        cout<<mask[i]<<" ";
     }
     cout<<"\n:";
     int num;
-    bool match=true;
-    for(int i=0;i<ARRAY_SIZE;i++)
+    
+    int userEntry[PIN_SIZE];
+    for(int i=0;i<PIN_SIZE;i++)
     {
-        cin>>num;
-        if(arr[i]!=num)
-        {
-            match=false;
-            break;
-        } 
-    }    
-    if(match)
+        cin>>userEntry[i];
+    }
+
+    if(verifyPin(PIN,userEntry,mask,PIN_SIZE, MASK_SIZE)==true)
     {
-        cout<<"Your PIN is correct";
+         cout<<"Your PIN is correct";
+
     }
     else
     {
-        cout<<"Your PIN is correct";
+        cout<<"Your PIN is not correct";
 
     }
     
