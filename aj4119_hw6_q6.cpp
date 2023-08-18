667435152806297753822fedf745ed462dfe6e88
pintuBeast <abhishek.jk1992@gmail.com>
2023-08-18 16:26:41 +0530

    q6 done

---
+++ aj4119_hw6_q6.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool isNum(string str);
+bool isNum(string str)
+{
+    bool isNumber=true;
+    for(int i=0;i<str.length();i++)
+    {
+        if((str[i]>='a'&& str[i]<='z') || (str[i]>='A'&& str[i]<='Z'))
+        {
+            isNumber=false;
+        }
+
+    }
+    return isNumber;
+}
+void maskNum(string str);
+void maskNum(string str)
+{
+        for(int i=0;i<str.length();i++)
+    {
+        cout<<"x";
+
+    }
+    cout<<" ";
+
+}
+void parseString(string str);
+void parseString(string str)
+{
+    string word="";
+    for(int i=0;i<=str.length();i++)
+    {
+        
+        bool isNumber=false;
+       // cout<<i<<" "<<(int)str[i]<<"\t";
+        if((int)str[i]==' ' || i == str.length())
+        {   
+            
+            if(isNum(word) == true)
+            {
+                maskNum(word);
+            }
+            else
+            {
+                cout<<word<<" ";
+            }
+            //cout<<word<<" "<<isNumber<<"\t";
+            word="";
+        }else
+        {
+            word +=str[i];
+            
+        }
+
+    }
+
+}
+int main()
+{
+    string str;
+    cout<<"Please enter a string:\n";
+    getline(cin,str);
+    parseString(str);
+    return 0;
+}
